d718cf40d5f6293e8c2d5e0fb42c0e32da3f7f34
rharidas18 <rharidas18@gmail.com>
2020-12-22 18:49:48 -0600

    Pagination added to the nest thermostat report

--- mynest/mynest/src/main/java/com/weatherize/reports/mynest/controller/ThermostatController.java
+++ mynest/mynest/src/main/java/com/weatherize/reports/mynest/controller/ThermostatController.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.weatherize.reports.mynest.annotation.CrossOriginsList;
@@ -23,11 +26,13 @@ public class ThermostatController {
 
 	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@RequestMapping("/thermostats")
-	public ResponseEntity<List<Thermostat>> getEntries() {
+	public ResponseEntity<List<Thermostat>> getEntries(@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "15") int size) {
 		try {
 			List<Thermostat> entries = new ArrayList<Thermostat>();
+			Pageable paging = PageRequest.of(page, size);
 
-			nestdataRepository.findAll().forEach(entries::add);
+			nestdataRepository.findAll(paging).forEach(entries::add);
 
 			if (entries.isEmpty()) {
 				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
