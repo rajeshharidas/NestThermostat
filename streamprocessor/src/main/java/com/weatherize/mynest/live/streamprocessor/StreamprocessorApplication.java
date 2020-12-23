11b9accb2450ed149835cb2e1f4f625b0bc46a58
rharidas18 <rharidas18@gmail.com>
2020-12-23 10:20:29 -0600

    kafka streams processor

---
+++ streamprocessor/src/main/java/com/weatherize/mynest/live/streamprocessor/StreamprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.weatherize.mynest.live.streamprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamprocessorApplication.class, args);
+	}
+
+}
---
+++ streamprocessor/src/test/java/com/weatherize/mynest/live/streamprocessor/StreamprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherize.mynest.live.streamprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
