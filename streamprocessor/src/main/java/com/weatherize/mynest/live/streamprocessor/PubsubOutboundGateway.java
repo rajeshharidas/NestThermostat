ec84039d481e76da8c7d3163dd05f584c018871b
rharidas18 <rharidas18@gmail.com>
2020-12-25 15:02:06 -0600

    removed potential security issues

--- streamprocessor/src/main/java/com/weatherize/mynest/live/streamprocessor/PubsubOutboundGateway.java
+++
@@ -1,9 +0,0 @@
-package com.weatherize.mynest.live.streamprocessor;
-
-import org.springframework.integration.annotation.MessagingGateway;
-
-@MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
-public interface PubsubOutboundGateway {
-
-	void sendToPubsub(String text);
-}
--- streamprocessor/src/main/java/com/weatherize/mynest/live/streamprocessor/StreamprocessorApplication.java
+++ streamprocessor/src/main/java/com/weatherize/mynest/live/streamprocessor/StreamprocessorApplication.java
@@ -7,89 +7,40 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gcp.pubsub.core.PubSubTemplate;
-import org.springframework.cloud.gcp.pubsub.integration.AckMode;
-import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundChannelAdapter;
-import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
-import org.springframework.cloud.gcp.pubsub.support.BasicAcknowledgeablePubsubMessage;
-import org.springframework.cloud.gcp.pubsub.support.GcpPubSubHeaders;
-import org.springframework.context.annotation.Bean;
-import org.springframework.integration.annotation.MessagingGateway;
-import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.channel.DirectChannel;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.MessageHandler;
-
 
 @SpringBootApplication
 public class StreamprocessorApplication implements CommandLineRunner {
 
-	public static Logger logger = LoggerFactory.getLogger(StreamprocessorApplication.class);
+	@Autowired
+	private KafkaTemplate<String, String> template;
 	
+	private final CountDownLatch latch = new CountDownLatch(3);
+	
+	public static Logger logger = LoggerFactory.getLogger(StreamprocessorApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(StreamprocessorApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		this.template.send("myTopic", "foo11");
+		this.template.send("myTopic", "foo12");
+		this.template.send("myTopic", "foo13");
+		latch.await(60, TimeUnit.SECONDS);
+		logger.info("All received");
 		
-	} 
+	}
 
-    @Autowired
-    private KafkaTemplate<String, String> template;
-
-    private final CountDownLatch latch = new CountDownLatch(3);
-    
-    @Bean
-    public MessageChannel pubsubInputChannel() {
-  	return new DirectChannel();
-    }
-    
-    @Bean
-    public PubSubInboundChannelAdapter messageChannelAdapter(
-  	  @Qualifier("pubsubInputChannel") MessageChannel inputChannel,
-  	  PubSubTemplate pubSubTemplate) {
-  	PubSubInboundChannelAdapter adapter =
-  		new PubSubInboundChannelAdapter(pubSubTemplate, "NestHVACEventSubscription");
-  	adapter.setOutputChannel(inputChannel);
-  	adapter.setAckMode(AckMode.MANUAL);
-  	logger.info("Inbound Channel Adapter is initialized");
-
-  	return adapter;
-    }
-    
-    @Bean
-    @ServiceActivator(inputChannel = "pubsubInputChannel")
-    public MessageHandler messageReceiver() {
-  	return message -> {
-  		logger.info("Message arrived! Payload: " + new String((byte[]) message.getPayload()));
-  	  BasicAcknowledgeablePubsubMessage originalMessage =
-  		message.getHeaders().get(GcpPubSubHeaders.ORIGINAL_MESSAGE, BasicAcknowledgeablePubsubMessage.class);
-  	  originalMessage.ack();
-  	};
-    }
-    
-    @Bean
-    @ServiceActivator(inputChannel = "pubsubOutputChannel")
-    public MessageHandler messageSender(PubSubTemplate pubsubTemplate) {
-  	return new PubSubMessageHandler(pubsubTemplate, "NestHVACEventTopic");
-    }
-        
-
-    @Override
-    public void run(String... args) throws Exception {
-        this.template.send("myTopic", "foo11");
-        this.template.send("myTopic", "foo12");
-        this.template.send("myTopic", "foo13");
-        latch.await(60, TimeUnit.SECONDS);
-        logger.info("All received");
-    }
-
-    @KafkaListener(topics = "myTopic")
-    public void listen(ConsumerRecord<?, ?> cr) throws Exception {
-        logger.info(cr.toString());
-        latch.countDown();
-    }
+	@KafkaListener(topics = "myTopic")
+	public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+		logger.info(cr.toString());
+		latch.countDown();
+	}
 }
